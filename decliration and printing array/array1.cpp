9ede2ecaf3a1c417462a8ecbd0d90799f8b5b930
Niks <106048768+NikhilMahabudhe@users.noreply.github.com>
2022-08-07 11:12:40 +0530

    Add files via upload

---
+++ decliration and printing array/array1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int array[10];
+   for(int i=0;i<10;i++){
+       cout<<"Enter your No:";
+       cin>>array[i];
+   }
+   for(int k=0;k<10;k++){
+     cout<<array[k]<<endl;
+     
+   }
+    return 0;
+}
